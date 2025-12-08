2cfcc6d9ddbb09d9e0a8631aa17205fc7daca8b2
NuRsUlTaN <nrsltankairbekov@gmail.com>
2025-12-08 16:28:27 +0500

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-// lab15/utils.js
-
-/**
- * Генерирует уникальный ID, основанный на метке времени.
- * @returns {string} Уникальный ID.
- */
-export function generateUniqueId() {
-    return Date.now().toString(); 
-}
